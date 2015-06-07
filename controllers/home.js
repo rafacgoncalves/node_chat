a00b434e2f5ed9a483dc8a417a0e7eaa49e0490c
unknown <rafael@RAFAEL-VAIO.expert.int>
2015-06-07 11:28:09 -0300

    Primeiro commit

---
+++ controllers/home.js
@@ -0,0 +1,32 @@
+module.exports = function(app) { 
+  var Usuario = app.models.usuario;
+  var HomeController = { 
+    index: function(req, res) { 
+      res.render('home/index'); 
+    }, 
+    login: function(req, res) { 
+      var query = {email: req.body.usuario.email}; 
+      Usuario.findOne(query).select('nome email').exec(
+        function(erro, usuario){ 
+          if (usuario) { 
+            req.session.usuario = usuario; 
+            res.redirect('/contatos'); 
+          } else { 
+            var usuario = req.body.usuario; 
+            Usuario.create(usuario, function(erro, usuario){ 
+              if(erro){ res.redirect('/'); 
+            } else { 
+              req.session.usuario = usuario; 
+              res.redirect('/contatos'); 
+            } 
+          }); 
+          } 
+        }); 
+    },
+    logout: function(req, res) {
+      req.session.destroy(); 
+      res.redirect('/');
+    } 
+  };
+  return HomeController; 
+};
